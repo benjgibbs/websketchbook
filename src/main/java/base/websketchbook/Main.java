d2f0974497350df675d73c176683388e286c8122
Ben Gibbs <benjgibbs@gmail.com>
2014-12-23 23:14:29 +0000

    improve padding and scaling

--- src/main/java/base/websketchbook/Main.java
+++ src/main/java/base/websketchbook/Main.java
@@ -7,15 +7,19 @@ import spark.Spark;
 import com.google.common.base.Strings;
 
 public class Main {
-    public static void main(String[] args) {
-	ProcessBuilder process = new ProcessBuilder();
-	String portStr = process.environment().get("PORT");
-	int port = 4567;
-	if(!Strings.isNullOrEmpty(portStr)){
-	    port = Integer.parseInt(portStr);
+	public static void main(String[] args) {
+		setPort();
+		staticFileLocation("/public");
+		get("/hello", (req, res) -> "Hello World");
 	}
-	Spark.port(port);
-	staticFileLocation("/public");
-	get("/hello", (req, res) -> "Hello World");
-    }
+
+	private static void setPort() {
+		ProcessBuilder process = new ProcessBuilder();
+		String portStr = process.environment().get("PORT");
+		int port = 4567;
+		if (!Strings.isNullOrEmpty(portStr)) {
+			port = Integer.parseInt(portStr);
+		}
+		Spark.port(port);
+	}
 }
