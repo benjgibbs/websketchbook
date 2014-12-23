08d3416c2739f00d0afddd5c3319274a9f0008ee
Ben Gibbs <benjgibbs@gmail.com>
2014-12-23 11:46:58 +0000

    Use the port specified by heroku

--- src/main/java/base/websketchbook/Main.java
+++ src/main/java/base/websketchbook/Main.java
@@ -1,11 +1,19 @@
 package base.websketchbook;
 
 import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import static spark.SparkBase.staticFileLocation;
+import spark.Spark;
+
+import com.google.common.base.Strings;
 
 public class Main {
     public static void main(String[] args) {
-	
+	String portStr = System.getProperty("process.env.PORT");
+	int port = 4567;
+	if(!Strings.isNullOrEmpty(portStr)){
+	    port = Integer.parseInt(portStr);
+	}
+	Spark.port(port);
 	staticFileLocation("/public");
 	get("/hello", (req, res) -> "Hello World");
     }
